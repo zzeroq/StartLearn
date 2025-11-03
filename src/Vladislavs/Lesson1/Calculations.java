461d03ce0d885b165eb5708822f82b08e04d1247
zzeroq <zzzerooq@gmail.com>
2025-11-03 15:41:20 +0200

    Changed calculations, init number to double number

--- src/Vladislavs/Lesson1/Calculations.java
+++ src/Vladislavs/Lesson1/Calculations.java
@@ -7,17 +7,22 @@ class Calculations {
         double number2 = 33.0;
         double number3 = 53.0;
 
-        double Multiply= number1 * number2 / number3;
+        double Multiply = number1 * number2 / number3;
         System.out.println("Multiply = " + Multiply);
 
-        double Subs= number1 - number2;
+        double Subs = number1 - number2;
         System.out.println("Subs = " + Subs);
 
-        double Sum= number1 + number2;
+        double Sum = number1 + number2;
         System.out.println("Sum = " + Sum);
 
         double Divide = number1 / number2;
         System.out.println("Divide = " + Divide);
+
+        int number1_0 =25;
+        int number2_0 =22;
+        int SumInt = number1_0 + number2_0;
+        System.out.println("SumInt = " + SumInt);
 
     }
 
