52ae336c9210f4809e84dd80f94a76d2ea18ba4e
zzeroq <zzzerooq@gmail.com>
2025-11-03 14:29:11 +0200

    Initial commit

---
+++ src/Vladislavs/Lesson1/Calculations.java
@@ -0,0 +1,26 @@
+package Vladislavs.Lesson1;
+
+class Calculations {
+    public static void main(String[] args) {
+
+        double number1 = 11.0;
+        double number2 = 33.0;
+
+        double Multiply= number1 * number2;
+        System.out.println("Multiply = " + Multiply);
+
+        double Subs= number1 - number2;
+        System.out.println("Subs = " + Subs);
+
+        double Sum= number1 + number2;
+        System.out.println("Sum = " + Sum);
+
+        double Divide = number1 / number2;
+        System.out.println("Divide = " + Divide);
+
+    }
+
+
+
+
+}
