da67522e6ad4c5694fa46fd9aecf1eccbec32bd5
zzeroq <zzzerooq@gmail.com>
2025-11-24 19:44:15 +0200

    Lesson 4, homework task 20!

--- src/vladislavs/uhovs/lesson4/homework/level7/task17/CalculatorTest.java
+++ src/vladislavs/uhovs/lesson4/homework/level7/task17/CalculatorTest.java
@@ -11,6 +11,8 @@ class CalculatorTest {
         calculatorTest.isEvenTest2();
     }
 
+    private final Calculator calculator = new Calculator();
+
     public void checkResult(String testName, int result, int expectedResult) {
         if (result == expectedResult) {
             System.out.println(testName + " = OK");
@@ -26,38 +28,32 @@ class CalculatorTest {
         }
     }
     public void sumTest() {
-        Calculator calculator = new Calculator();
         int result = calculator.sum(35, 40);
         checkResult("Sum test", result, 75);
 
     }
 
     public void subTest() {
-        Calculator calculator = new Calculator();
         int result = calculator.sub(35, 15);
         checkResult("Sub test", result, 20);
     }
 
     public void mulTest() {
-        Calculator calculator = new Calculator();
         int result = calculator.mul(5, 5);
         checkResult("Multiply test", result, 25);
     }
 
     public void divTest() {
-        Calculator calculator = new Calculator();
         int result = calculator.div(40, 8);
         checkResult("Divide test", result, 5);
     }
 
     public void isEvenTest1() {
-        Calculator calculator = new Calculator();
         boolean result = calculator.isEven(40);
         checkResultBoolean("Is even test", result, true);
     }
 
     public void isEvenTest2() {
-        Calculator calculator = new Calculator();
         boolean result = calculator.isEven(41);
         checkResultBoolean("Not even test", result, false);
     }
---
+++ src/vladislavs/uhovs/lesson4/homework/level7/task17/LightColorDetectorTest.java
@@ -0,0 +1,57 @@
+package vladislavs.uhovs.lesson4.homework.level7.task17;
+
+public class LightColorDetectorTest {
+    public static void main(String[] args) {
+        LightColorDetectorTest lightColorDetector = new LightColorDetectorTest();
+
+        lightColorDetector.testViolet(405);
+        lightColorDetector.testBlue(475);
+        lightColorDetector.testGreen(505);
+        lightColorDetector.testYellow(585);
+        lightColorDetector.testOrange(619);
+        lightColorDetector.testRed(620);
+        lightColorDetector.testInvisible(760);
+    }
+
+    public void checkResult(String testName, String result, String expectedResult){
+
+
+        if (result.equals(expectedResult)) {
+            System.out.println(testName + " = OK");
+        } else {
+            System.out.println(testName + " = FAIL");
+        }
+
+    }
+
+    private final LightColorDetector lightColorDetector = new LightColorDetector();
+
+    public void testViolet(int wavelength) {
+        String result = lightColorDetector.detect(wavelength);
+        checkResult("Test violet color", result, "Violet");
+    }
+    public void testBlue(int wavelength) {
+        String result = lightColorDetector.detect(wavelength);
+        checkResult("Test blue color", result, "Blue");
+    }
+    public void testGreen(int wavelength) {
+        String result = lightColorDetector.detect(wavelength);
+        checkResult("Test green color", result, "Green");
+    }
+    public void testYellow(int wavelength) {
+        String result = lightColorDetector.detect(wavelength);
+        checkResult("Test yellow color", result, "Yellow");
+    }
+    public void testOrange(int wavelength) {
+        String result = lightColorDetector.detect(wavelength);
+        checkResult("Test orange color", result, "Orange");
+    }
+    public void testRed(int wavelength) {
+        String result = lightColorDetector.detect(wavelength);
+        checkResult("Test red color", result, "Red");
+    }
+    public void testInvisible(int wavelength) {
+        String result = lightColorDetector.detect(wavelength);
+        checkResult("Test invisible light color", result, "Invisible Light");
+    }
+}
